4290b778c57c55ea9a3c214c0aace7115d8bacdc
Marcelo Chabán <Marcelochaban@gmail.com>
2023-05-09 17:08:35 -0300

    Creation exercise openBootCamp 3

--- Practica_java/OpenBootCamp3/src/Main.java
+++ Practica_java/OpenBootCamp3/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Persona person=new Persona();
+        person.setNombre("Marcelo");
+        person.setEdad(20);
+        person.setTelefono("1545986545");
+        System.out.println( "Persona{" +
+                "edad=" + person.getEdad() +
+                ", nombre='" + person.getNombre() + '\'' +
+                ", telefono='" +person.getTelefono()+ '\'');
     }
 }
